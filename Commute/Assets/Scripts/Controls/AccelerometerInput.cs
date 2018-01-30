386f26382d357e478ba0434fa297d4173708c5c7
Ricardo Desmedt <ricardodesmedt@MacBook-Pro-van-Ricardo.local>
2018-01-30 22:58:22 +0100

    [ADD] added a resource pack with some simple textures. Added simple tilt controls. Added touch controls and started touch helper class

---
+++ Commute/Assets/Scripts/Controls/AccelerometerInput.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AccelerometerInput : MonoBehaviour {
+
+    [SerializeField]
+    Text accelerationXText;
+
+    [SerializeField]
+    Text amountOfFingers;
+
+    [SerializeField]
+    Text amplifier;
+
+    [SerializeField]
+    [Range(0.5f, 10f)]
+    float accelerationAmplifier = 1f; // Determines how strong the acceleration input should be mapped onto the object
+
+
+    private void Start()
+    {
+        Debug.Log(Input.simulateMouseWithTouches);
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        // Translate the gameObject according to the devices orientation.
+        transform.Translate(Input.acceleration.x * accelerationAmplifier, 0, 0);
+
+        foreach (Touch touch in Input.touches)
+        {
+            if (TouchControls.TouchOnLeftSide(touch)) {
+                accelerationAmplifier -= 0.2f;
+            } else if (TouchControls.TouchOnRightSide(touch)) {
+                accelerationAmplifier += 0.2f;
+            }
+        }
+
+
+        accelerationXText.text = "Acceleration.x = " + Input.acceleration.x;
+        amountOfFingers.text = "Amount of fingers = " + Input.touchCount;
+        amplifier.text = "Amplifier = " + accelerationAmplifier;
+	}
+}
